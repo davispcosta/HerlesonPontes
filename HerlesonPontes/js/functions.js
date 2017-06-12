b03fa2fd8c83b627f60be8c595c035f52cdb1d2e
davispcosta <davispcosta@gmail.com>
2017-06-12 00:36:52 -0300

    Add files via upload

---
+++ HerlesonPontes/js/functions.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+      $(".button-collapse").sideNav();  
+      $('.collapsible').collapsible();
+    
+    
+    $(document).ready(function () {
+            $("#owl").owlCarousel({
+                navigation: false,
+                slideSpeed: 100,
+                paginationSpeed: 800,
+                singleItem: true,
+                autoPlay: true
+            });
+        });
+})
+
